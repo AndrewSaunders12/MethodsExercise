51cc80d8a762cd05f3c5c8363b73a481cd3bb16e
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-03-25 17:22:32 -0400

    Redid the assignment

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,38 +1,40 @@
-﻿namespace MethodsExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var amountOfCars = Sum(2, 6);
-            var cookies = Multiply(4, 7);
-            Console.WriteLine($" I have {amountOfCars} cars.");
-            Console.WriteLine($" My sister and I love cookies, I ate {cookies}");
-        Console.WriteLine("What is your name?"); // output
-            var name = Console.ReadLine(); // input 
-            Console.WriteLine("What is you favorite color?"); // output
-            var color = Console.ReadLine(); // input
-            Console.WriteLine("What is your favorite animal?"); // output
-            var animal = Console.ReadLine(); // input
-            Console.WriteLine("Whos your favorite band?"); // output
-            var band = Console.ReadLine(); // input 
 
+            var pickles = Sum(10, 11);
+            var bugs = multiply(17, 18);
 
-            Console.WriteLine($" {name} was having a bad day because his {color} shirt got ripped because of his {animal}." +
-                $" It got better when {name} got to see his favorite band {band}.");
+            Console.WriteLine("What is your name?"); // output
+            var name = Console.ReadLine(); // input
+            Console.WriteLine("What is your favorite ice cream flavor?"); // output
+            var flavor = Console.ReadLine(); // input
+            Console.WriteLine("Who is your favorite superhero"); // output
+            var hero = Console.ReadLine(); // input
+            Console.WriteLine("Which planet is third from the sun?"); // output
+            var orb = Console.ReadLine(); // input
+
+            Console.WriteLine($" {name} loves {flavor} and his favorite superhero is {hero} he is the strongest on the {orb}");
+            Console.WriteLine();
+            Console.WriteLine($" ill be {pickles} in two years. And {bugs} is a big number.");
+
         }
-
         public static int Sum(int num1, int num2)
         {
             var answer = num1 + num2;
             return answer;
         }
 
-        public static int Multiply(int num1, int num2)
+        public static int multiply(int num1, int num2)
         {
             return num1 * num2;
         }
-        
-        
-    }        
+
+    }
 }
+
